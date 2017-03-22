f77a2849d6761cd7b3ec8a69af93dc8b840664da
Roman <kaboom.ua@gmail.com>
2017-03-22 22:58:33 +0200

    Update setCommentID.js

--- src/middlewares/setCommentID.js
+++ src/middlewares/setCommentID.js
@@ -2,11 +2,13 @@ import {ADD_COMMENT} from '../constants'
 
 export default store => next => action => {
 
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     if (action.type != ADD_COMMENT) {
         return next(action)
     }
 
     let state = store.getState(),
+        //завязка на length - очень плохая логика, что если удалят коммент?
         comment_count = state.comments.length
 
     next({...action, comment_id: comment_count})
